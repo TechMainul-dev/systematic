e3debeae37b5a0694b1b5e680418b94f5523b709
TechMainul <nilakashkhan2@gmail.com>
2021-10-08 06:05:42 +0600

    systematic upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// BX Slider======================
+$(document).ready(function () {
+    $('.slider_area').bxSlider();
+    $('header nav').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+});
